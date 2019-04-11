f807f18985bbbc42d00e185fbf8c8eff1531b026
DirkTrautweinUrano <48240393+DirkTrautweinUrano@users.noreply.github.com>
2019-04-11 10:49:13 +0200

    Update Files_Folder.ps1

--- Files_Folder.ps1
+++ Files_Folder.ps1
@@ -1,2 +1,3 @@
+# Prüfen ob ein Ordner existiert und anlegen falls dieser nicht existiert.
 $targetPath = "Pfad der geprüft und ggf. angelegt werden soll."
 if (-not (Test-Path $targetPath -PathType Container)){ $null = mkdir $targetPath }
